00a21163685ee31af898df5cd0c3e12ce11bfc31
abdulaziz-juramirzayev <templeuzb@gmail.com>
2022-03-11 18:28:48 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,12 @@
+function Inner(){
+    document.getElementById('hello').innerHTML = "Alik";
+
+
+}
+function sole(){
+    console.log = "Mana javaScriptni ham boshlavoldikku !";
+}
+
+function myf(){
+    alert("Hello");
+}
